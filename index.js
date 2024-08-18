3d629802e13700896a38639b5e7892707794bab3
mathewalexKerala <mathewalexmasai@gmail.com>
2024-08-18 18:03:31 +0530

    commited sdfert

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/:date', function(req, res) {
   let dateParam = req.params.date;
   let unixVal, dateVal;
 if(dateParam===null){
-  res.json({unix:new Date().toUTCString(),utc:new Date()})
+  res.json({unix:new Date(),utc:new Date()})
 }
   // Check if the dateParam is a number (Unix timestamp)
   if (!isNaN(dateParam) && dateParam.length >= 10) {
